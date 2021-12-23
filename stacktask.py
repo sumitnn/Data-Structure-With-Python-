dedb214eb6f501e60e6a8ae98900bf6bd8e5582b
sumitnn <sumitnautiyal446@gmail.com>
2021-12-23 13:09:43 +0530

    first-commit

---
+++ stacktask.py
@@ -0,0 +1,6 @@
+# # Check for balanced parentheses in Python
+# Input: {[]{()}}
+# Output: Balanced
+
+# Input: [{}{}(]
+# Output: Unbalanced
---
+++ towerofhanoi.py
@@ -0,0 +1,10 @@
+# Tower of Hanoi is a mathematical puzzle where we have three rods and n disks.
+# Without Recursion
+
+def Tower(disk, start, middle, end):
+
+    print(f"")
+    pass
+
+
+Tower(3, 'A', 'B', 'C')
